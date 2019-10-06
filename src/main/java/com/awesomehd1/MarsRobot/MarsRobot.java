f34f59e185bbf8b39fa837467b1c5622ceb7b7a7
Edison <earthgood84@gmail.com>
2019-10-06 07:43:48 -0700

    seems ok lol

--- src/main/java/com/awesomehd1/MarsRobot/MarsRobot.java
+++ src/main/java/com/awesomehd1/MarsRobot/MarsRobot.java
@@ -1,4 +1,21 @@
 package com.awesomehd1.MarsRobot;
 
 public class MarsRobot {
+    String Status;
+    int Speed;
+    float Temperature;
+    int temperture;
+    private String status;
+
+    void CheckTemperature() {
+
+        if (temperture < -80) {
+            status = "returning home";
+            Speed = 5;
+        }
+    }
+
+    void showAttributes () {
+
+    }
 }
